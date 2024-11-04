10140de28b03382d3836f125aaa25883cf2d997f
Mark Roenfeldt <1187productions@gmail.com>
2024-11-04 07:05:30 -0700

    uploading first batch of test project

---
+++ C_Sharp/CodeWithMosh_C_Sharp_TESTING/TestNinja/TestNinja.UnitTests/CostumerControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CostumerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>()); // NotFound object
+
+            //Assert.That(result, Is.InstanceOf<NotFound>()); // NotFound or one of its derivatives
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(1);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
