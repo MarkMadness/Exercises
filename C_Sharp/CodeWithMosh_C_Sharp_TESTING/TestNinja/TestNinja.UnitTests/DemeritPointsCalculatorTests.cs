fb17161984643b9893bff91cc75d5eddab0aac8a
Mark Roenfeldt <1187productions@gmail.com>
2024-11-05 08:52:17 -0700

    Stack.cs unit testing uploaded

---
+++ C_Sharp/CodeWithMosh_C_Sharp_TESTING/TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    /// <summary>
+    /// Summary description for DemeritPointsCalculatorTests
+    /// </summary>
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _dpc;
+        [SetUp]
+        public void SetUp()
+        {
+            _dpc = new DemeritPointsCalculator();
+        }
+        [Test]
+        public void CalculateDemeritPoints_SpeedLessThanZero_ThrowArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => _dpc.CalculateDemeritPoints(-1));
+        }
+        [Test]
+        public void CalculateDemeritPoints_SpeedGreaterThan300_ThrowArgumentOutOfRangeException()
+        {
+            Assert.That(() => _dpc.CalculateDemeritPoints(301), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(1, 0)]
+        [TestCase(50, 0)]
+        [TestCase(66, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        [TestCase(80, 3)]
+        [TestCase(200, 27)]
+        public void CalculateDemeritPoints_SpeedGreaterThanSpeedLimit_ReturnDemeritPoint(int speed, int expectedResult)
+        {
+            var result = _dpc.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ C_Sharp/CodeWithMosh_C_Sharp_TESTING/TestNinja/TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,106 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void Push_WhenCalled_AddsObjectToStack()
+        {
+            var stack = new Stack<int>();
+
+            stack.Push(1);
+            var count = stack.Count;
+
+            Assert.That(count, Is.EqualTo(1));
+        }
+        [Test]
+        public void Push_NullPassed_ThrowArgumentNullException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Pop_WhenCalled_RemovesObjectFromStack()
+        {
+            var stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Pop();
+            var count = stack.Count;
+
+            Assert.That(count, Is.EqualTo(1));
+        }
+        [Test]
+        [Ignore("My solution. Test below is better")]
+        public void Pop_CountIsZero_ThrowsInvalidOperationException()
+        {
+            var stack = new Stack<int>();
+
+            var count = stack.Count;
+
+            Assert.That(() => count.Equals(0), Throws.InvalidOperationException);
+        }
+        [Test]
+        public void Pop_StackIsEmpty_ThrowsInvalidOperationException()
+        {
+            var stack = new Stack<int>();
+
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_WhenCalled_ReturnLastElementInStack()
+        {
+            var stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            var peek = stack.Peek();
+
+            Assert.That(peek, Is.EqualTo(2));
+        }
+        [Test]
+        public void Peek_CountIsZero_ThrowInvalidOperationException()
+        {
+            var stack = new Stack<int>();
+
+            // Assert.That(() => stack.Count.Equals(0), Throws.InvalidOperationException);
+            Assert.That(() => stack.Peek(), Throws.InvalidOperationException);
+        }
+        [Test]
+        public void Peek_StackPopulated_ReturnObjectOnTopOfStack()
+        {
+            var stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+
+            stack.Pop();
+
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+        [Test]
+        public void Peek_StackPopulated_DoesNotRemoveObjectOnTopOfStack()
+        {
+            var stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+
+            Assert.That(stack.Peek(), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Count_EmptyStack_ReturnZero()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(stack.Count, Is.EqualTo(0));
+        }
+    }
+}
